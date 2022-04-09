49b85d30a20873058904b921ff529b86329a3195
chethanahj <chethana0016@gmail.com>
2022-04-09 17:29:15 +0530

    szcdzvcdsv

---
+++ project/src/test/java/project/One.java
@@ -0,0 +1,16 @@
+package project;
+
+
+public class One {
+	
+	public static void main(String[] args) {
+		
+	}
+	public void entervaluesTest3() {
+		System.out.println("entered values successfully");
+		
+		
+		
+	}
+
+}
